f97e4007a7e98875d76e49212b1ee9a0ed58c93a
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2018-04-19 00:25:46 +0900

    detail

--- sample.js
+++ sample.js
@@ -1,25 +1,26 @@
 const fs = require('fs');
-var content;
+const markdown = require('markdown').markdown;
 const worksPath = "./resources/works/2018yuko/";
-var dirs;
+
 $(_ => {
-  console.log("first");
   fs.readdir(worksPath, function(err, files) {
     if(err) {
       throw err;
     }
-    console.log(files[0]);
+    $('.Entries').append('<div>' + files + '</div>');
     files.forEach(function(element) {
-      console.log(element);
-      //dirs += element.toString();
-      dirs = files;
-      $('h1').text('Yukari')
-        .append('<p>dir : ' + dirs + '</p>')
-        .on('click', function(e) {
-          $(this).hide();
+      let path = worksPath + element + '/README.md';
+      console.log(path);
+      try {
+        fs.readFile(path, 'utf-8', (err, data) => {
+          if(err) throw err;
+          console.log(data.toString());
+          $('.Entries').append('<div class="Markdown">' + markdown.toHTML(data.toString()) + '</div>');
         });
+      } catch(e) {
+        console.log(e);
+      }
     });
   })
-  console.log("second");
 
 })
