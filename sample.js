8f08500853f9a2f9bc50e81f6fb9395f90f5b04a
sugichan0116 <sugichan0116@gmail.com>
2018-04-18 20:38:48 +0900

    modify

--- sample.js
+++ sample.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 var content;
-
+const worksPath = "./resources/works/2018yuko/";
+var dirs;
 $(_ => {
-  try {
-    let json = fs.readFileSync('./resources/works/2018yuko/test.json', 'utf-8');
-    content = JSON.parse(json);
-  } catch(err) {
-
-  }
-  $('h1').text('Yukari')
-    .append('<p>star : ' + content.stars + '</p>')
-    .on('click', function(e) {
-      $(this).hide();
+  console.log("first");
+  fs.readdir(worksPath, function(err, files) {
+    if(err) {
+      throw err;
+    }
+    console.log(files[0]);
+    files.forEach(function(element) {
+      console.log(element);
+      //dirs += element.toString();
+      dirs = files;
+      $('h1').text('Yukari')
+        .append('<p>dir : ' + dirs + '</p>')
+        .on('click', function(e) {
+          $(this).hide();
+        });
     });
+  })
+  console.log("second");
+
 })
