b9e327fa21011df578fe5c0ef549dcb6832bcfef
sugichan0116 <sugichan0116@gmail.com>
2018-04-18 19:23:01 +0900

    semantic

--- sample.js
+++ sample.js
@@ -1,7 +1,15 @@
-$(_ => {
+const fs = require('fs');
+var content;
 
+$(_ => {
+  try {
+    let json = fs.readFileSync('./resources/works/2018yuko/test.json', 'utf-8');
+    content = JSON.parse(json);
+  } catch(err) {
+
+  }
   $('h1').text('Yukari')
-    .append('<p>child</p>')
+    .append('<p>star : ' + content.stars + '</p>')
     .on('click', function(e) {
       $(this).hide();
     });
