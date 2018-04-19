11504f1061338b87d1eb669905c8026e39738cc2
sugichan0116 <sugichan0116@gmail.com>
2018-04-19 18:35:27 +0900

    test

--- sample.js
+++ sample.js
@@ -1,8 +1,31 @@
 const fs = require('fs');
 const markdown = require('markdown').markdown;
 const worksPath = "./resources/works/2018yuko/";
+var $card, $cards;
+var $description;
+
+function readCard() {
+  //カードの読み込み
+  fs.readFile("./card.html", "utf-8", (err, data) => {
+    if(err) throw err;
+    $card = $(data);
+    $cards = $('<div class="ui horizontal segments">').append($card).append($card);
+    $card.on("click", (event) => {
+      console.log($(this));
+      $(this).after("<div>");
+    })
+    $('.Entries').append($cards);
+  });
+}
 
 $(_ => {
+  //デスクリプションの読み込み
+  fs.readFile("./description.html", "utf-8", function (err, data) {
+    if(err) throw err;
+    $description = $(data);
+    readCard();
+  });
+
   fs.readdir(worksPath, function(err, files) {
     if(err) {
       throw err;
@@ -10,7 +33,6 @@ $(_ => {
     $('.Entries').append('<div>' + files + '</div>');
     files.forEach(function(element) {
       let path = worksPath + element + '/README.md';
-      console.log(path);
       try {
         fs.readFile(path, 'utf-8', (err, data) => {
           if(err) throw err;
