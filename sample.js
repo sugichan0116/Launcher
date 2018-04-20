6e24875c8f798737c13f6dd378d9f32e6997fa1c
sugichan0116 <sugichan0116@gmail.com>
2018-04-20 14:11:09 +0900

    fixed

--- sample.js
+++ sample.js
@@ -1,48 +1,35 @@
 const fs = require('fs');
 const markdown = require('markdown').markdown;
 const worksPath = "./resources/works/2018yuko/";
-var $card, $cards;
-var $description;
+var card; //string
+var description;
+var dirs;
 
-function readCard() {
-  //カードの読み込み
-  fs.readFile("./card.html", "utf-8", (err, data) => {
-    if(err) throw err;
-    $card = $(data);
-    $cards = $('<div class="ui horizontal segments">').append($card).append($card);
-    $card.on("click", (event) => {
-      console.log($(this));
-      $(this).after("<div>");
-    })
-    $('.Entries').append($cards);
-  });
-}
+card = fs.readFileSync("./card.html", "utf-8");
+description = fs.readFileSync("./description.html", "utf-8");
 
-$(_ => {
-  //デスクリプションの読み込み
-  fs.readFile("./description.html", "utf-8", function (err, data) {
-    if(err) throw err;
-    $description = $(data);
-    readCard();
-  });
-
+function ReadDir() {
   fs.readdir(worksPath, function(err, files) {
     if(err) {
       throw err;
     }
-    $('.Entries').append('<div>' + files + '</div>');
-    files.forEach(function(element) {
-      let path = worksPath + element + '/README.md';
-      try {
-        fs.readFile(path, 'utf-8', (err, data) => {
-          if(err) throw err;
-          console.log(data.toString());
-          $('.Markdown').append(markdown.toHTML(data.toString()));
-        });
-      } catch(e) {
-        console.log(e);
-      }
-    });
+    dirs = files;
+    console.log(dirs);
   })
+}
+
+//ready
+$(_ => {
+  //load
+  ReadDir();
+
+  //event
+  if(card == null) console.log("nullpo");
+
+  //delegate
+  //ramda can't get this
+  $('.Entries').on('click', '.Entry', function() {
+    $(this).parent().after($(description));
+  });
 
 })
