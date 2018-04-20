184c6dc80fb9ba3c0af7b8f912dcb7995a2e189a
sugichan0116 <sugichan0116@gmail.com>
2018-04-20 22:08:08 +0900

    load dir

--- sample.js
+++ sample.js
@@ -3,10 +3,14 @@ const markdown = require('markdown').markdown;
 const worksPath = "./resources/works/2018yuko/";
 var card; //string
 var description;
-var dirs;
+var dirs = {};
 
 card = fs.readFileSync("./card.html", "utf-8");
 description = fs.readFileSync("./description.html", "utf-8");
+
+function GetSettings(dir) {
+  return JSON.parse(fs.readFileSync(worksPath + dir + "/setting.json"));
+}
 
 function ReadDir() {
   fs.readdir(worksPath, function(err, files) {
@@ -15,6 +19,25 @@ function ReadDir() {
     }
     dirs = files;
     console.log(dirs);
+
+    //promise 使いたい
+    dirs.forEach(function(data) {
+      let $entry = $(card);
+      let path = worksPath + data + "/";
+      let settings = GetSettings(data);
+      let tagHTML = "";
+      settings.tags.split(' ').forEach(function(data) {
+        tagHTML += '<a class="ui tag label">' + data + '</a>'
+      });
+      $entry.find('.header').append(settings.name);
+      $entry.find('.Tags').append(tagHTML);
+      $entry.find('.Snapshot').attr("src", path + settings.snapshot);
+      $entry.find('.Time').append(settings.time);
+      $entry.find('.Difficulty').append(settings.difficulty);
+      $entry.attr('dir', data);
+      $('.Entries').append($entry);
+    });
+
   })
 }
 
@@ -23,13 +46,29 @@ $(_ => {
   //load
   ReadDir();
 
-  //event
-  if(card == null) console.log("nullpo");
+  console.log(dirs);
 
   //delegate
   //ramda can't get this
-  $('.Entries').on('click', '.Entry', function() {
-    $(this).parent().after($(description));
-  });
+  $('.Entries')
+    .on('click', '.Entry', function() {
+      const dir = $(this).attr('dir');
+      const path = worksPath + dir + "/";
+      const settings = GetSettings(dir);
+      let tagHTML = "";
+      settings.tags.split(' ').forEach(function(data) {
+        tagHTML += '<a class="ui tag label">' + data + '</a>'
+      });
 
+      let $desc = $(description).addClass('transition').addClass('hidden');
+      $desc.find('.header').append(settings.name);
+      $desc.find('.Tags').append(tagHTML);
+      $desc.find('.Snapshot').attr("src", path + settings.snapshot);
+      $desc.find('.Difficulty').append(settings.difficulty);
+      $desc.find('.Time').append(settings.time);
+
+      $('.hidden').remove();
+      $(this).parent().after($desc);
+      $('.Description').transition('fade right');
+    });
 })
